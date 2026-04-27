6e4988de6c4bd3866c7128d992d7039b946dd67e
ABC COMPANY POHARKA <ajaxsihag@gmail.com>
2026-04-27 14:06:27 +0530

    ABC WRA

--- ABCWRA.ps1
+++ ABCWRA.ps1
@@ -1,9 +1,9 @@
 # Enable TLSv1.2 for compatibility with older clients
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
 
-$DownloadURL = 'https://naeembolchhi.github.io/WinRAR-Activator/WRA-20240608011948.cmd'
+$DownloadURL = 'https://github.com/ABCCOMPANYPOHARKA/ABC-WRA/blob/main/ABC%20WRA.cmd'
 
-$FilePath = "$env:TEMP\WRA.cmd"
+$FilePath = "$env:TEMP\ABCWRA.cmd"
 
 try {
     Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing -OutFile $FilePath
