21e8362058cf442c63abb06dfcadc5e4bb1cf15f
ABC COMPANY POHARKA <120773766+ABCCOMPANYPOHARKA@users.noreply.github.com>
2026-04-27 14:30:11 +0530

    Update ABCWRA.ps1

--- ABCWRA.ps1
+++ ABCWRA.ps1
@@ -1,7 +1,7 @@
 # Enable TLSv1.2 for compatibility with older clients
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
 
-$DownloadURL = 'https://github.com/ABCCOMPANYPOHARKA/ABC-WRA/blob/main/ABCWRA.ps1'
+$DownloadURL = 'https://github.com/ABCCOMPANYPOHARKA/ABC-WRA/blob/main/ABCWRA.cmd'
 
 $FilePath = "$env:TEMP\ABCWRA.cmd"
 
