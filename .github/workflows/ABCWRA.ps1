fefa9148519963f8f3adc2f61ce94ceab0f55f62
ABC COMPANY POHARKA <120773766+ABCCOMPANYPOHARKA@users.noreply.github.com>
2026-04-27 15:04:51 +0530

    Add files via upload

---
+++ .github/workflows/ABCWRA.ps1
@@ -0,0 +1,19 @@
+# Enable TLSv1.2 for compatibility with older clients
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
+
+$DownloadURL = 'https://github.com/ABCCOMPANYPOHARKA/ABC-WRA/blob/main/ABCWRA.ps1'
+
+$FilePath = "$env:TEMP\ABCWRA.cmd"
+
+try {
+    Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing -OutFile $FilePath
+} catch {
+    Write-Error $_
+	Return
+}
+
+if (Test-Path $FilePath) {
+    Start-Process $FilePath -Wait
+    $item = Get-Item -LiteralPath $FilePath
+    $item.Delete()
+}
